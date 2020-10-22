e3977db45314820a363af621d1fcba283996e1c3
Biditmangal <biditmangal9506@gmail.com>
2020-10-22 23:24:01 +0530

    Added MD5 algorithm

---
+++ MD5/MD5.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+	uint64_t size;			// Size of input in bytes
+	uint32_t buffer[4];		// Current accumulation of hash
+	uint8_t input[64];		// Input to be used in the next step
+	uint8_t digest[16];		// Result of algorithm
+} md5_context;
+
+void md5_init(md5_context *ctx);
+void md5_update(md5_context *ctx, uint8_t *input, size_t input_len);
+void md5_finalize(md5_context *ctx);
+void md5_steps(uint32_t *buffer, uint32_t *input);
+
+uint8_t* md5_string(char *input);
+uint8_t* md5_file(FILE *file);
+
+uint32_t F(uint32_t X, uint32_t Y, uint32_t Z);
+uint32_t G(uint32_t X, uint32_t Y, uint32_t Z);
+uint32_t H(uint32_t X, uint32_t Y, uint32_t Z);
+uint32_t I(uint32_t X, uint32_t Y, uint32_t Z);
+
+uint32_t rotate_left(uint32_t x, uint32_t n);
+
+void print_bytes(void *p, size_t length);
+void print_hash(uint8_t *p);
---
+++ MD5/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "md5.h"
+
+int main(int argc, char *argv[]){
+	uint8_t *result;
+	if(argc > 1){
+		for(int i = 1; i < argc; ++i){
+			result = md5_string(argv[i]);
+			print_hash(result);
+		}
+	}
+	else{
+		result = md5_file(stdin);
+		print_hash(result);
+	}
+
+	free(result);
+}
