994c5993f965208c75a50544f5269d70bb87ddd6
Biditmangal <biditmangal9506@gmail.com>
2020-11-27 23:43:36 +0530

    Completed SHAGenerator

--- Authsystem/authsystem.c
+++ Authsystem/authsystem.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h>
+// #include <unistd.h>
+#include <io.h>
 #include <string.h>
-#include "sha256.c"
 #include "sha256.h"
 #define SALTMAX 6
-#define PASSMAX 10
+#define PASSMIN 10
+#define USERMAX 20
+
 char tmp_s[];
 void gen_random(const int len) {
 
@@ -16,27 +18,22 @@ void gen_random(const int len) {
 
     srand((unsigned) time(NULL) * getpid());
 
-
     for (int i = 0; i < len; ++i)
         tmp_s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
 
 }
-void shaGenerator(char *hashstring)
+void shaGenerator(CHAR *hashstring)
 {
 
 	CHAR buf[SHA256_BLOCK_SIZE];
 	SHA256_Context context;
-	CHAR text1[10000];
-
-	printf("Enter a string:\n");
-	scanf("%s\n", &text1);
 
 	sha256_init(&context);
-	sha256_update(&context, text1, strlen(text1));
+	sha256_update(&context, hashstring, strlen(hashstring));
 	sha256_final(&context, buf);
 
 
-	printf("\n\n\n\nString: %s\n", text1 );
+	printf("String: %s\n", hashstring );
 	printf("SHA-256 hash: ");
 	for(unsigned int i = 0; i < SHA256_BLOCK_SIZE; ++i){
   		printf("%02x", buf[i]);
@@ -47,14 +44,25 @@ void shaGenerator(char *hashstring)
 
 int main() {
     //char password[] = "aa";
-    CHAR password[PASSMAX];
-    for(int i=0; i < PASSMAX; ++i){
-    scanf("%c", &password[i]);
-  }
+    CHAR password[PASSMIN];
+    CHAR username[USERMAX];
+    // for(int i=0; i < PASSMIN; ++i){
+    // scanf("%c", &password[i]);
+  	// }
+	printf("Username: " );
+	scanf("%s",username);
+	printf("Password: " );
+	scanf("%s",password);
+
+
     gen_random(SALTMAX);
+
     strcat(password, tmp_s);
-    printf("%s \n", tmp_s);
+
+	printf("%s \n", tmp_s);
+    printf("%s \n", username);
     printf("%s \n", password);
+
     shaGenerator(password);
     return 0;
 }
